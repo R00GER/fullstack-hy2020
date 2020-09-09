c3ee883441d37194f08624d7f0f3aa7003d33480
R00GER <roni.friman@sonit.fi>
2020-09-09 21:48:56 +0300

    Refaktorointi

--- osa4/index.js
+++ osa4/index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const app = require('./app');
 
-const config = require('./utils/config');
-const logger = require('./utils/logger');
+const { PORT } = require('./utils/config');
+const { info } = require('./utils/logger');
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+server.listen(PORT, () => {
+  info(`Server running on port ${PORT}`);
 });
